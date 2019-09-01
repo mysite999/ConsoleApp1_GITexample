8f9306308b1fc397d54ef7464ccc6ddc23428b97
mysite999 <aro91@yandex.ru>
2019-09-01 15:26:39 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1_GITexample
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, Ara!!!!!!");
+            Console.WriteLine("+++++++++++++");
             Console.ReadKey();
         }
     }
