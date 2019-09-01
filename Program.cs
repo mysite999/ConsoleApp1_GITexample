271a22c706624fd4cb60dcf2556c6b69639d5a8c
mysite999 <aro91@yandex.ru>
2019-09-01 16:37:17 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1_GITexample
         static void Main(string[] args)
         {
            
-            Console.WriteLine("+++++");
+            Console.WriteLine("----");
             
         }
     }
